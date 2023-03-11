1cb2645ddd0a32f4becbe514bccd1c2a1fd173b5
solak1 <rohanasdf12@gmail.com>
2023-03-11 21:56:09 +0800

    C# 多國語系

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,8 @@
 using Blog;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var requestCulture = new RequestCulture("en-us", "en-us");
+
+CultureInfo.DefaultThreadCurrentCulture = requestCulture.UICulture;         // WebAssembly Culture、UICulture 必須要一樣 https://docs.microsoft.com/zh-tw/aspnet/core/blazor/globalization-localization?view=aspnetcore-6.0&pivots=webassembly
+CultureInfo.DefaultThreadCurrentUICulture = requestCulture.UICulture;      
+var a = CookieRequestCultureProvider.DefaultCookieName;
 await builder.Build().RunAsync();
