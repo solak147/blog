820bd85d248fcf57aed03fb651882aaf719eeded
solak1 <rohanasdf12@gmail.com>
2023-03-15 00:18:56 +0800

    mobile nav

--- Blog/wwwroot/js/javascript.js
+++ Blog/wwwroot/js/javascript.js
@@ -3,3 +3,17 @@
         hljs.highlightElement(el);
     });
 }
+
+function navToggle() {
+
+    let wd = $(".sidebar").width(); 
+    if (wd > 0) {
+        $(".sidebar").animate({
+            width: 0
+        });
+    } else {
+        $(".sidebar").animate({
+            width: '70vw'
+        });
+    }
+}
