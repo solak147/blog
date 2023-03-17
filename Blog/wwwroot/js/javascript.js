284c7db04423051c0872e1b8839bb493e12cb1ba
solak1 <rohanasdf12@gmail.com>
2023-03-18 00:26:37 +0800

    xx

--- Blog/wwwroot/js/javascript.js
+++ Blog/wwwroot/js/javascript.js
@@ -10,13 +10,17 @@ function navToggle() {
         $(".sidebar").animate({
             width: 0
         });
-        
+
+        $('.close').addClass('menu');
+        $('.close').removeClass('close');
         $('body').css('overflow-y', 'auto');
     } else {
         $(".sidebar").animate({
             width: '70vw'
         });
-   
+
+        $('.menu').addClass('close');
+        $('.menu').removeClass('menu');
         $('body').css('overflow-y', 'hidden');
     }
     $('.navbar-brand').toggle();
