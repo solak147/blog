86a72194210e315c35c896715230bc1bb3b3dac3
solak147 <rohanasdf12@gmail.com>
2023-03-25 14:15:49 +0000

    Deploying to gh-pages from  @ 160420215f121376302caba2da93b4b1a57dcb4c 🚀

--- js/javascript.js
+++ js/javascript.js
@@ -16,14 +16,16 @@ function navToggle() {
         $('.close').addClass('menu');
         $('.close').removeClass('close');
         $('body').css('overflow-y', 'auto');
-        document.getElementsByTagName("main")[0].removeEventListener('touchmove', mo, { passive: false });
+        $('html').css('overflow-y', 'auto');
+        //document.getElementsByTagName("main")[0].removeEventListener('touchmove', mo, { passive: false });
     } else {
         $(".sidebar").animate({ "left": "0" }, "slow");
 
         $('.menu').addClass('close');
         $('.menu').removeClass('menu');
         $('body').css('overflow-y', 'hidden');
-        document.getElementsByTagName("main")[0].addEventListener('touchmove', mo, { passive: false });
+        $('html').css('overflow-y', 'hidden');
+        //document.getElementsByTagName("main")[0].addEventListener('touchmove', mo, { passive: false });
     }
 
 }
